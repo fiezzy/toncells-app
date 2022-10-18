d9d0ab18525b643d36554183ddc913cc09e2b9a6
sepezho <sepezho@gmail.com>
2022-10-18 18:09:24 +0200

    ?

--- src/App.tsx
+++ src/App.tsx
@@ -18,13 +18,13 @@ import UserModal from "./components/UserModal";
 import SomethingWentWrong from "./components/SomethingWentWrong";
 import { message } from "antd";
 import {
-  NftIcon,
-  CellsWrapperX,
-  IconsX,
-  CellsWrapperY,
-  IconsY,
-  ZoomWrapper,
-  RootContainer,
+	NftIcon,
+	CellsWrapperX,
+	IconsX,
+	CellsWrapperY,
+	IconsY,
+	ZoomWrapper,
+	RootContainer,
 } from "./style";
 import CellModalDefault from "./components/CellModalDefault";
 import imga from "./nftitems/1.png";
@@ -37,282 +37,287 @@ import React from "react";
 export const nftIcons: string[] = [];
 
 for (let i = 1; i < 26; i++) {
-  nftIcons.push(`${NFT_ICONS + i}.png`);
+	nftIcons.push(`${NFT_ICONS + i}.png`);
 }
 
 const App: VFC = () => {
-  const [bigArr, setBigArr] = useState();
-  const [isBuyMode, setIsBuyMode] = useState<boolean>(false);
-  const [isZoomMode, setIsZoomMode] = useState<boolean>(false);
-  const [onSideBar, setonSideBar] = useState<boolean>(false);
-  const [mapVersion, setMapVersion] = useState<number>(1);
-  const [isDescMode, toggleDescMode] = useState<boolean>(false);
-  const [hex, setHex] = useState<string>("");
-  const [isInvoiceMode, setIsInvoiceMode] = useState<boolean>(false);
-  const [selectedCells, updateSelectedCells] = useState<number[]>([]);
-  const [isBuyALotMode, setBuyALotMode] = useState<boolean>(false);
-  const [cellsAreaData, setSelectedAreas] = useState<any[]>([]);
-  const [isConnectWalletMode, setIsConnectWalletMode] =
-    useState<boolean>(false);
-  const [isUserModalMode, setIsUserModalMode] = useState<boolean>(false);
+	const [bigArr, setBigArr] = useState();
+	const [isBuyMode, setIsBuyMode] = useState<boolean>(false);
+	const [isZoomMode, setIsZoomMode] = useState<boolean>(false);
+	const [onSideBar, setonSideBar] = useState<boolean>(false);
+	const [mapVersion, setMapVersion] = useState<number>(1);
+	const [isDescMode, toggleDescMode] = useState<boolean>(false);
+	const [hex, setHex] = useState<string>("");
+	const [isInvoiceMode, setIsInvoiceMode] = useState<boolean>(false);
+	const [selectedCells, updateSelectedCells] = useState<number[]>([]);
+	const [isBuyALotMode, setBuyALotMode] = useState<boolean>(false);
+	const [cellsAreaData, setSelectedAreas] = useState<any[]>([]);
+	const [isConnectWalletMode, setIsConnectWalletMode] =
+		useState<boolean>(false);
+	const [isUserModalMode, setIsUserModalMode] = useState<boolean>(false);
 
-  const [actualMaps, setActualMaps] = useState<string[]>([
-    ApiMaps.Default,
-    ApiMaps.Edit,
-  ]);
+	const [actualMaps, setActualMaps] = useState<string[]>([
+		ApiMaps.Default,
+		ApiMaps.Edit,
+	]);
 
-  const cellsCollection = generateCellsCollection();
+	const cellsCollection = generateCellsCollection();
 
-  // TODO - ПРИВЕСТИ ЭТО ВСЕ В НОРМАЛЬНЫЙ ВИД
-  const getMaps = useCallback(async () => {
-    try {
-      const fetchDefaultMap = await fetch(ApiMaps.Default);
-      const fetchEditMap = await fetch(ApiMaps.Edit);
+	// TODO - ПРИВЕСТИ ЭТО ВСЕ В НОРМАЛЬНЫЙ ВИД
+	const getMaps = useCallback(async () => {
+		try {
+			const fetchDefaultMap = await fetch(ApiMaps.Default);
+			const fetchEditMap = await fetch(ApiMaps.Edit);
 
-      const requestDefaultMap = await fetchDefaultMap.blob();
-      const requestEditMap = await fetchEditMap.blob();
+			const requestDefaultMap = await fetchDefaultMap.blob();
+			const requestEditMap = await fetchEditMap.blob();
 
-      const defaultMapImageObjectURL = URL.createObjectURL(requestDefaultMap);
-      const editMapImageObjectURL = URL.createObjectURL(requestEditMap);
+			const defaultMapImageObjectURL = URL.createObjectURL(requestDefaultMap);
+			const editMapImageObjectURL = URL.createObjectURL(requestEditMap);
 
-      setActualMaps([defaultMapImageObjectURL, editMapImageObjectURL]);
-    } catch (error) {
-      message.error(`${error}`);
-    }
-  }, []);
-  const [selectedImage, setSelectedImage] = useState(null);
+			setActualMaps([defaultMapImageObjectURL, editMapImageObjectURL]);
+		} catch (error) {
+			message.error(`${error}`);
+		}
+	}, []);
+	const [selectedImage, setSelectedImage] = useState(null);
 
-  useEffect(() => {
-    setTimeout(function fetchMaps() {
-      getMaps();
-      setTimeout(fetchMaps, 30000);
-    }, 30000);
-  }, [getMaps]);
+	useEffect(() => {
+		setTimeout(function fetchMaps() {
+			getMaps();
+			setTimeout(fetchMaps, 30000);
+		}, 30000);
+	}, [getMaps]);
 
-  useEffect(() => {
-    (async () => {
-      setBigArr(await GetStatus());
+	useEffect(() => {
+		(async () => {
+			setBigArr(await GetStatus());
 
-      setTimeout(async function fetchBigArr() {
-        setBigArr(await GetStatus());
+			setTimeout(async function fetchBigArr() {
+				setBigArr(await GetStatus());
 
-        setTimeout(fetchBigArr, 30000);
-      }, 30000);
-    })();
-  }, []);
+				setTimeout(fetchBigArr, 30000);
+			}, 30000);
+		})();
+	}, []);
 
-  const toggleBuyALotMode = useCallback(() => {
-    if (isBuyALotMode) {
-      updateSelectedCells([]);
-      setSelectedAreas([]);
-    }
-    if (!hex) {
-      setBuyALotMode((prev) => !prev);
-    } else {
-      message.error("Finish last invoice!", 10);
-    }
-  }, [isBuyALotMode, hex]);
+	const toggleBuyALotMode = useCallback(() => {
+		if (isBuyALotMode) {
+			updateSelectedCells([]);
+			setSelectedAreas([]);
+		}
+		if (!hex) {
+			setBuyALotMode((prev) => !prev);
+		} else {
+			message.error("Finish last invoice!", 10);
+		}
+	}, [isBuyALotMode, hex]);
 
-  const buyAreas = () => {
-    setIsInvoiceMode((prev) => !prev);
-    setBuyALotMode((prev) => !prev);
-    setSelectedAreas([]);
-  };
+	const buyAreas = () => {
+		setIsInvoiceMode((prev) => !prev);
+		setBuyALotMode((prev) => !prev);
+		setSelectedAreas([]);
+	};
 
-  const setSelectedCells = (e: any) => {
-    updateSelectedCells(e);
-  };
+	const setSelectedCells = (e: any) => {
+		updateSelectedCells(e);
+	};
 
-  const { isCellModalActive } = useContext(CellModalContext);
+	const { isCellModalActive } = useContext(CellModalContext);
 
-  const toggleBuyMode = useCallback(() => {
-    setIsBuyMode((prev) => !prev);
-  }, []);
+	const toggleBuyMode = useCallback(() => {
+		setIsBuyMode((prev) => !prev);
+	}, []);
 
-  const toggleMap = (mapold: any) => {
-    let newMap = mapold + 1;
-    if (newMap === 2) newMap = 0;
+	const toggleMap = (mapold: any) => {
+		let newMap = mapold + 1;
+		if (newMap === 2) newMap = 0;
 
-    setMapVersion(newMap);
-  };
+		setMapVersion(newMap);
+	};
 
-  useEffect(() => {
-    const saved = localStorage.getItem("invoiceData");
-    if (!!saved) {
-      const initialValue = JSON.parse(saved);
-      if (!!initialValue.hex) {
-        setHex(initialValue.hex);
-        setSelectedCells(initialValue.ids);
-      }
-    }
-  }, []);
+	useEffect(() => {
+		const saved = localStorage.getItem("invoiceData");
+		if (!!saved) {
+			const initialValue = JSON.parse(saved);
+			if (!!initialValue.hex) {
+				setHex(initialValue.hex);
+				setSelectedCells(initialValue.ids);
+			}
+		}
+	}, []);
 
-  console.log(bigArr);
+	console.log(bigArr);
 
-  const toggleInvoiceMode = useCallback(() => {
-    setIsInvoiceMode((prev) => !prev);
-  }, []);
+	const toggleInvoiceMode = useCallback(() => {
+		setIsInvoiceMode((prev) => !prev);
+	}, []);
 
-  const toggleConnectWalletMode = useCallback(() => {
-    setIsConnectWalletMode((prev) => !prev);
-  }, []);
+	const toggleConnectWalletMode = useCallback(() => {
+		setIsConnectWalletMode((prev) => !prev);
+	}, []);
 
-  const toggleUserModalMode = useCallback(() => {
-    setIsUserModalMode((prev) => !prev);
-  }, []);
+	const toggleUserModalMode = useCallback(() => {
+		setIsUserModalMode((prev) => !prev);
+	}, []);
 
-  const nftItems = nftIcons.map((src) => (
-    <NftIcon key={src} src={src} alt="#" />
-  ));
+	const nftItems = nftIcons.map((src) => (
+		<NftIcon key={src} src={src} alt="#" />
+	));
 
-  const GlobalStyle = createGlobalStyle`
+	const GlobalStyle = createGlobalStyle`
 
   	body {
 	    overflow: ${isCellModalActive ? "hidden" : "auto"};
 	  }
   `;
 
-  console.log(isCellModalActive);
+	console.log(isCellModalActive);
 
-  const OffScroll = createGlobalStyle<{ isOff: boolean }>`
+	const OffScroll = createGlobalStyle<{ isOff: boolean }>`
       body {
         overflow: ${({ isOff }) => (isOff ? "hidden" : "scroll")};
       }
   `;
 
-  return (
-    <>
-      <GlobalStyle />
-      {selectedImage && (
-        <div>
-          <img
-            alt="not fount"
-            width={"250px"}
-            src={URL.createObjectURL(selectedImage)}
-          />
-          <br />
-          <button onClick={() => setSelectedImage(null)}>Remove</button>
-        </div>
-      )}
+	return (
+		<>
+			<GlobalStyle />
+			{selectedImage && (
+				<div>
+					<img
+						alt="not fount"
+						width={"250px"}
+						src={URL.createObjectURL(selectedImage)}
+					/>
+					<br />
+					<button onClick={() => setSelectedImage(null)}>Remove</button>
+				</div>
+			)}
 
-      <canvas id="canvas" width={"16"} height={"16"}></canvas>
-      <span id="color"></span>
+			<canvas
+				id="canvas"
+				width={"16"}
+				height={"16"}
+				style={{ opacity: "0" }}></canvas>
+			<span style={{ opacity: "0" }} id="color"></span>
 
-      <input
-        type="file"
-        name="myImage"
-        onChange={(event: any) => {
-          setSelectedImage(event.target.files[0]);
-        }}
-      />
+			<input
+				type="file"
+				name="myImage"
+				style={{ opacity: "0" }}
+				onChange={(event: any) => {
+					setSelectedImage(event.target.files[0]);
+				}}
+			/>
 
-      <Container>
-        <DockBar
-          bigArr={bigArr}
-          toggleBuyMode={toggleBuyMode}
-          toggleZoomMode={(isZoom: boolean) => setIsZoomMode(isZoom)}
-          isZoomMode={isZoomMode}
-          setonSideBar={(isSideBarActive: boolean) =>
-            setonSideBar(isSideBarActive)
-          }
-          toggleMap={() => toggleMap(mapVersion)}
-          toggleDescMode={() => toggleDescMode((prev) => !prev)}
-          hex={hex}
-          toggleInvoiceMode={toggleInvoiceMode}
-          toggleBuyALotMode={toggleBuyALotMode}
-          isBuyALotMode={isBuyALotMode}
-          buyAreas={buyAreas}
-          toggleConnectWalletMode={toggleConnectWalletMode}
-          toggleUserModalMode={toggleUserModalMode}
-        />
+			<Container>
+				<DockBar
+					bigArr={bigArr}
+					toggleBuyMode={toggleBuyMode}
+					toggleZoomMode={(isZoom: boolean) => setIsZoomMode(isZoom)}
+					isZoomMode={isZoomMode}
+					setonSideBar={(isSideBarActive: boolean) =>
+						setonSideBar(isSideBarActive)
+					}
+					toggleMap={() => toggleMap(mapVersion)}
+					toggleDescMode={() => toggleDescMode((prev) => !prev)}
+					hex={hex}
+					toggleInvoiceMode={toggleInvoiceMode}
+					toggleBuyALotMode={toggleBuyALotMode}
+					isBuyALotMode={isBuyALotMode}
+					buyAreas={buyAreas}
+					toggleConnectWalletMode={toggleConnectWalletMode}
+					toggleUserModalMode={toggleUserModalMode}
+				/>
 
-        {isBuyMode && (
-          <>
-            <OffScroll isOff={isBuyMode} />
-            <NftViewer
-              isBuyMode={isBuyMode}
-              bigArr={bigArr}
-              toggleBuyMode={toggleBuyMode}
-            />
-          </>
-        )}
+				{isBuyMode && (
+					<>
+						<OffScroll isOff={isBuyMode} />
+						<NftViewer
+							isBuyMode={isBuyMode}
+							bigArr={bigArr}
+							toggleBuyMode={toggleBuyMode}
+						/>
+					</>
+				)}
 
-        {isDescMode && (
-          <DescModeModal
-            isDescMode={isDescMode}
-            toggleDescMode={() => toggleDescMode((prev) => !prev)}
-          />
-        )}
+				{isDescMode && (
+					<DescModeModal
+						isDescMode={isDescMode}
+						toggleDescMode={() => toggleDescMode((prev) => !prev)}
+					/>
+				)}
 
-        {isInvoiceMode && (
-          <>
-            <OffScroll isOff={isInvoiceMode} />
-            <CellModalInvoice
-              isVisible={isInvoiceMode}
-              onClose={toggleInvoiceMode}
-              cellIds={selectedCells}
-              hex={hex}
-              setHex={setHex}
-              setSelectedCells={setSelectedCells}
-            />
-          </>
-        )}
+				{isInvoiceMode && (
+					<>
+						<OffScroll isOff={isInvoiceMode} />
+						<CellModalInvoice
+							isVisible={isInvoiceMode}
+							onClose={toggleInvoiceMode}
+							cellIds={selectedCells}
+							hex={hex}
+							setHex={setHex}
+							setSelectedCells={setSelectedCells}
+						/>
+					</>
+				)}
 
-        {isConnectWalletMode && (
-          <>
-            <OffScroll isOff={isConnectWalletMode} />
-            <ConnectWalletModal
-              isVisible={isConnectWalletMode}
-              onClose={toggleConnectWalletMode}
-              toggleUserModalMode={toggleUserModalMode}
-            />
-          </>
-        )}
+				{isConnectWalletMode && (
+					<>
+						<OffScroll isOff={isConnectWalletMode} />
+						<ConnectWalletModal
+							isVisible={isConnectWalletMode}
+							onClose={toggleConnectWalletMode}
+							toggleUserModalMode={toggleUserModalMode}
+						/>
+					</>
+				)}
 
-        {isUserModalMode && (
-          <>
-            <OffScroll isOff={isUserModalMode} />
-            <UserModal
-              cellsData={bigArr}
-              isVisible={isUserModalMode}
-              onClose={toggleUserModalMode}
-              toggleConnectWalletMode={toggleConnectWalletMode}
-              cellsCollection={cellsCollection}
-            />
-          </>
-        )}
+				{isUserModalMode && (
+					<>
+						<OffScroll isOff={isUserModalMode} />
+						<UserModal
+							cellsData={bigArr}
+							isVisible={isUserModalMode}
+							onClose={toggleUserModalMode}
+							toggleConnectWalletMode={toggleConnectWalletMode}
+							cellsCollection={cellsCollection}
+						/>
+					</>
+				)}
 
-        <RootContainer isZoomMode={isZoomMode}>
-          <ZoomWrapper isZoomMode={isZoomMode}>
-            <CellsWrapperX>
-              <IconsX>{nftItems}</IconsX>
-              <CellsWrapperY>
-                <IconsY>{nftItems}</IconsY>
-                <Cells
-                  actualMaps={actualMaps}
-                  isZoomMode={isZoomMode}
-                  onSideBar={onSideBar}
-                  mapVersion={mapVersion}
-                  nftImgs={nftIcons}
-                  bigArr={bigArr}
-                  setHex={setHex}
-                  hex={hex}
-                  setSelectedCells={setSelectedCells}
-                  selectedCells={selectedCells}
-                  isInvoiceMode={isInvoiceMode}
-                  toggleInvoiceMode={toggleInvoiceMode}
-                  isBuyALotMode={isBuyALotMode}
-                  cellsAreaData={cellsAreaData}
-                  setSelectedAreas={setSelectedAreas}
-                  cellsCollection={cellsCollection}
-                />
-              </CellsWrapperY>
-            </CellsWrapperX>
-          </ZoomWrapper>
-        </RootContainer>
-      </Container>
-    </>
-  );
+				<RootContainer isZoomMode={isZoomMode}>
+					<ZoomWrapper isZoomMode={isZoomMode}>
+						<CellsWrapperX>
+							<IconsX>{nftItems}</IconsX>
+							<CellsWrapperY>
+								<IconsY>{nftItems}</IconsY>
+								<Cells
+									actualMaps={actualMaps}
+									isZoomMode={isZoomMode}
+									onSideBar={onSideBar}
+									mapVersion={mapVersion}
+									nftImgs={nftIcons}
+									bigArr={bigArr}
+									setHex={setHex}
+									hex={hex}
+									setSelectedCells={setSelectedCells}
+									selectedCells={selectedCells}
+									isInvoiceMode={isInvoiceMode}
+									toggleInvoiceMode={toggleInvoiceMode}
+									isBuyALotMode={isBuyALotMode}
+									cellsAreaData={cellsAreaData}
+									setSelectedAreas={setSelectedAreas}
+									cellsCollection={cellsCollection}
+								/>
+							</CellsWrapperY>
+						</CellsWrapperX>
+					</ZoomWrapper>
+				</RootContainer>
+			</Container>
+		</>
+	);
 };
 
 export default App;
--- src/components/CellEditModal/components/CellEditColorBlock/index.tsx
+++ src/components/CellEditModal/components/CellEditColorBlock/index.tsx
@@ -3,167 +3,166 @@ import { ColorPicker, useColor } from "react-color-palette";
 import { rgbToHex } from "../../../../utils/rgbToHex";
 import { Spin } from "antd";
 import {
-  LoadingOutlined,
-  ClearOutlined,
-  UploadOutlined,
+	LoadingOutlined,
+	ClearOutlined,
+	UploadOutlined,
 } from "@ant-design/icons";
 import {
-  ColorsWrapper,
-  SaveBtn,
-  CurrentColorWrapper,
-  CurrentColorBlock,
-  ClearAllBtn,
-  HiddenInput,
+	ColorsWrapper,
+	SaveBtn,
+	CurrentColorWrapper,
+	CurrentColorBlock,
+	ClearAllBtn,
+	HiddenInput,
 } from "./style";
 
 type Props = {
-  isEdit: boolean;
-  currentHex: string;
-  handleSavePixelsData: () => void;
-  isGettingSignature: boolean;
-  setCurrentHex: (hex: any) => void;
-  clearAllImage: () => void;
-  copyImage: () => void;
-  pasteImage: () => void;
-  uploadImage: (image: string) => void;
+	isEdit: boolean;
+	currentHex: string;
+	handleSavePixelsData: () => void;
+	isGettingSignature: boolean;
+	setCurrentHex: (hex: any) => void;
+	clearAllImage: () => void;
+	copyImage: () => void;
+	pasteImage: () => void;
+	uploadImage: (image: string) => void;
 };
 
 const CellEditColorBlock: VFC<Props> = (props) => {
-  const {
-    isEdit,
-    currentHex,
-    handleSavePixelsData,
-    isGettingSignature,
-    setCurrentHex,
-    clearAllImage,
-    copyImage,
-    pasteImage,
-    uploadImage,
-  } = props;
+	const {
+		isEdit,
+		currentHex,
+		handleSavePixelsData,
+		isGettingSignature,
+		setCurrentHex,
+		clearAllImage,
+		copyImage,
+		pasteImage,
+		uploadImage,
+	} = props;
 
-  const [color, setColor] = useColor("hex", currentHex);
-  const [uploadedImageInHex, setUploadedImageInHex] = useState<string>("");
+	const [color, setColor] = useColor("hex", currentHex);
+	const [uploadedImageInHex, setUploadedImageInHex] = useState<string>("");
 
-  const hiddenInput = useRef<HTMLInputElement>(null);
+	const hiddenInput = useRef<HTMLInputElement>(null);
 
-  const hasCopiedImage =
-    localStorage.getItem("copiedImage") !== null &&
-    localStorage.getItem("copiedImage") !== undefined;
+	const hasCopiedImage =
+		localStorage.getItem("copiedImage") !== null &&
+		localStorage.getItem("copiedImage") !== undefined;
 
-  console.log(hasCopiedImage);
+	console.log(hasCopiedImage);
 
-  const handleFileInputChange = (uploadedImage: File) => {
-    // let uploadedImageInHexStr;
+	const handleFileInputChange = (uploadedImage: File) => {
+		// let uploadedImageInHexStr;
 
-    let img = new Image();
-    img.src = uploadedImage ? URL.createObjectURL(uploadedImage) : "";
+		let img = new Image();
+		img.src = uploadedImage ? URL.createObjectURL(uploadedImage) : "";
 
-    let canvas = document.getElementById("canvas") as any;
-    let ctx = canvas.getContext("2d");
+		let canvas = document.getElementById("canvas") as any;
+		let ctx = canvas.getContext("2d");
 
-    img.onload = function () {
-      ctx.drawImage(img, 0, 0);
-      console.log("let pixel = : ", ctx.getImageData(16, 16, 1, 1));
-      let data = [] as any;
+		img.onload = function () {
+			ctx.drawImage(img, 0, 0, 16, 16);
+			console.log("let pixel = : ", ctx.getImageData(16, 16, 1, 1));
+			let data = [] as any;
 
-      for (let i = 0; i < 255; i++) {
-        let pixel = ctx.getImageData(i % 16, Math.floor(i / 16), 1, 1);
+			for (let i = 0; i < 255; i++) {
+				let pixel = ctx.getImageData(i % 16, Math.floor(i / 16), 1, 1);
 
-        data = [...data, pixel.data];
+				data = [...data, pixel.data];
 
-        if (i === 254) {
-          const hexStr = data
-            .map((e: any) => rgbToHex(e[0], e[1], e[2]))
-            .join("");
+				if (i === 254) {
+					const hexStr = data
+						.map((e: any) => rgbToHex(e[0], e[1], e[2]))
+						.join("");
 
-          uploadImage(hexStr);
-        }
-      }
-    };
-  };
+					uploadImage(hexStr);
+				}
+			}
+		};
+	};
 
-  const handleUploadClick = () => {
-    hiddenInput.current!.click();
-  };
+	const handleUploadClick = () => {
+		hiddenInput.current!.click();
+	};
 
-  return (
-    <>
-      <div>
-        <ColorsWrapper isEdit={isEdit}>
-          {isEdit ? (
-            <ColorPicker
-              width={322}
-              height={150}
-              color={color}
-              onChange={isEdit ? setColor : () => {}}
-              onChangeComplete={
-                isEdit ? (color) => setCurrentHex(color.hex) : () => {}
-              }
-              hideRGB
-              hideHSV
-            />
-          ) : (
-            <ColorPicker
-              width={322}
-              height={150}
-              color={color}
-              onChange={isEdit ? setColor : () => {}}
-              onChangeComplete={
-                isEdit ? (color) => setCurrentHex(color.hex) : () => {}
-              }
-              hideRGB
-              hideHSV
-              hideHEX
-            />
-          )}
-        </ColorsWrapper>
-        {isEdit && (
-          <CurrentColorWrapper>
-            <CurrentColorBlock color={currentHex} />
-            <ClearAllBtn onClick={clearAllImage}>
-              <ClearOutlined />
-            </ClearAllBtn>
-          </CurrentColorWrapper>
-        )}
-      </div>
+	return (
+		<>
+			<div>
+				<ColorsWrapper isEdit={isEdit}>
+					{isEdit ? (
+						<ColorPicker
+							width={322}
+							height={150}
+							color={color}
+							onChange={isEdit ? setColor : () => {}}
+							onChangeComplete={
+								isEdit ? (color) => setCurrentHex(color.hex) : () => {}
+							}
+							hideRGB
+							hideHSV
+						/>
+					) : (
+						<ColorPicker
+							width={322}
+							height={150}
+							color={color}
+							onChange={isEdit ? setColor : () => {}}
+							onChangeComplete={
+								isEdit ? (color) => setCurrentHex(color.hex) : () => {}
+							}
+							hideRGB
+							hideHSV
+							hideHEX
+						/>
+					)}
+				</ColorsWrapper>
+				{isEdit && (
+					<CurrentColorWrapper>
+						<CurrentColorBlock color={currentHex} />
+						<ClearAllBtn onClick={clearAllImage}>
+							<ClearOutlined />
+						</ClearAllBtn>
+					</CurrentColorWrapper>
+				)}
+			</div>
 
-      {isGettingSignature ? (
-        <SaveBtn color={currentHex} isGettingSignature={isGettingSignature}>
-          <Spin indicator={<LoadingOutlined />} style={{ color: "#000" }} />
-        </SaveBtn>
-      ) : (
-        <>
-          <HiddenInput
-            ref={hiddenInput}
-            type="file"
-            name="myImage"
-            onChange={(event: any) => {
-              handleFileInputChange(event.target.files[0]);
-            }}
-          />
-          {!isEdit && (
-            <SaveBtn color={currentHex} onClick={handleUploadClick}>
-              <UploadOutlined />
-              UPLOAD IMAGE
-            </SaveBtn>
-          )}
+			{isGettingSignature ? (
+				<SaveBtn color={currentHex} isGettingSignature={isGettingSignature}>
+					<Spin indicator={<LoadingOutlined />} style={{ color: "#000" }} />
+				</SaveBtn>
+			) : (
+				<>
+					<HiddenInput
+						ref={hiddenInput}
+						type="file"
+						name="myImage"
+						onChange={(event: any) => {
+							handleFileInputChange(event.target.files[0]);
+						}}
+					/>
+					{!isEdit && (
+						<SaveBtn color={currentHex} onClick={handleUploadClick}>
+							<UploadOutlined />
+							UPLOAD IMAGE
+						</SaveBtn>
+					)}
 
-          {!isEdit && (
-            <SaveBtn
-              color={currentHex}
-              onClick={hasCopiedImage ? pasteImage : copyImage}
-            >
-              {hasCopiedImage ? "PASTE" : "COPY"}
-            </SaveBtn>
-          )}
+					{!isEdit && (
+						<SaveBtn
+							color={currentHex}
+							onClick={hasCopiedImage ? pasteImage : copyImage}>
+							{hasCopiedImage ? "PASTE" : "COPY"}
+						</SaveBtn>
+					)}
 
-          <SaveBtn onClick={handleSavePixelsData} color={currentHex}>
-            {isEdit ? "SAVE" : "EDIT"}
-          </SaveBtn>
-        </>
-      )}
-    </>
-  );
+					<SaveBtn onClick={handleSavePixelsData} color={currentHex}>
+						{isEdit ? "SAVE" : "EDIT"}
+					</SaveBtn>
+				</>
+			)}
+		</>
+	);
 };
 
 export default CellEditColorBlock;
